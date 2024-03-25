0e02b085b5a8b049e46a96bee4c12421d2f0673c
Ovanesyanaz <ovanesyanaz@mail.ru>
2024-03-25 12:09:30 +0300

    new route

--- server/server.py
+++ server/server.py
@@ -1,17 +1,22 @@
 from flask import Flask
 from flask import send_file
+from flask import request
 import base64
+import time
 app = Flask(__name__)
 
 
-@app.route("/members")
-def members():
-    return {"members": ["members1", "members2", "members3"]}
+@app.route('/test', methods=["POST"])
+def test():
+    data = request.get_json()
+    print(data)
+    return {"answ" : str((int(data[0]) + int(data[1])))}
 
 
 
-@app.route('/get_image')
+@app.route('/get_image', methods = ["POST"])
 def get_image():
+    time.sleep(2)
     filename = "img/bogom_dan.jpg"
     img_in_bytes = str(base64.b64encode(open(filename, "rb").read()))[2:-6]
     return {"img_in_bytes" : img_in_bytes}
